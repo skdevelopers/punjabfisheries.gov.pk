c81aaaa5f20d34cb4921e6a18e497cb519df0c54
Muhammad Salman <skdevelopers@live.com>
2025-08-23 15:30:44 +0500

    admin merger commit By Salman

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Piniastudio',
+            'email' => 'help@piniastudio.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
         ]);
     }
 }
